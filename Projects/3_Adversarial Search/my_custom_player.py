61d28ae16aa61d25f1ccd4e753550a56b47eacc0
Mirko Mastrelli <mirko.mastrelli@gmail.com>
2018-06-20 17:20:44 +0200

    Added custom heuristics

--- Projects/3_Adversarial Search/my_custom_player.py
+++ Projects/3_Adversarial Search/my_custom_player.py
@@ -1,5 +1,7 @@
 
 from sample_players import DataPlayer
+import math
+import random
 
 
 class CustomPlayer(DataPlayer):
@@ -45,16 +47,18 @@ class CustomPlayer(DataPlayer):
         # EXAMPLE: choose a random move without any search--this function MUST
         #          call self.queue.put(ACTION) at least once before time expires
         #          (the timer is automatically managed for you)
-        depth_limit = 10
 
-        for depth in range(1, depth_limit + 1):
-            action = self.alpha_beta(state, depth)
-            if action is not None:
-                self.queue.put(action)        
-       
+        depth_limit = 10
+        if state.ply_count ==0:
+            self.queue.put(random.choice(state.actions()))
+        else:
+            for depth in range(1, depth_limit + 1):
+               action = self.alpha_beta(state, depth)
+               if action is not None:
+                   self.queue.put(action)
+              
     def minimax_decision(self, state, depth):
-        
-        
+                
         best_score = float("-inf")
         best_move = None
         for a in state.actions():
@@ -77,41 +81,31 @@ class CustomPlayer(DataPlayer):
     
         return best_move
         
-        
-        
-    
     def min_value(self, state, alpha, beta, depth):
         
         if depth <= 0:
-            return self.score(state)
-        
+            return self.heuristic3(state)        # Switch score with heuristic1 / heuristic2 / heuristic3
         if state.terminal_test():
             return state.utility(self.player_id)
-
         v = float("inf")
         for a in state.actions():
             v = min(v, self.max_value(state.result(a), alpha, beta, depth-1))
             if v <= alpha:
                 return v
-            beta = min(beta, v)
-            
+            beta = min(beta, v)            
         return v
 
     def max_value(self, state, alpha, beta, depth):
         if depth <= 0:
-            return self.score(state)
-        
+            return self.heuristic3(state)       # Switch score with heuristic1 and heuristic2
         if state.terminal_test():
-            return state.utility(self.player_id)
-        
-    
+            return state.utility(self.player_id)            
         v = float("-inf")
         for a in state.actions():
             v = max(v, self.min_value(state.result(a), alpha, beta, depth-1))
             if v >= beta:
                 return v
-            alpha = max(alpha, v)
-            
+            alpha = max(alpha, v)            
         return v
   
     def score(self, state):
@@ -121,3 +115,58 @@ class CustomPlayer(DataPlayer):
         own_liberties = state.liberties(own_loc)
         opp_liberties = state.liberties(opp_loc)
         return len(own_liberties) - len(opp_liberties)
+    
+    def heuristic1(self, state):   
+        """
+        This heuristic will be more aggressive than heuristic1, the player distance add aggressiveness
+        """
+        own_loc = state.locs[self.player_id]
+        opp_loc = state.locs[1 - self.player_id]
+        own_moves_minus_opp_moves = self.score(state)
+        own_distance_from_center= self.distance_from_center(self.coordinates(own_loc))
+        return own_moves_minus_opp_moves - own_distance_from_center
+    
+    def heuristic2(self, state):   
+        """
+        This heuristic will be more aggressive than heuristic1 the player distance reduces aggressiveness
+        """
+        own_loc = state.locs[self.player_id]
+        opp_loc = state.locs[1 - self.player_id]
+        players_distance = self.euclidean_distance(self.coordinates(own_loc), self.coordinates(opp_loc))
+        own_moves_minus_opp_moves = self.score(state)
+        own_distance_from_center= self.distance_from_center(self.coordinates(own_loc))
+        return own_moves_minus_opp_moves - own_distance_from_center - players_distance
+    
+    def heuristic3(self, state):   
+        """
+        This heuristic will be more aggressive than heuristic1 the player distance reduces aggressiveness
+        """
+        own_loc = state.locs[self.player_id]
+        opp_loc = state.locs[1 - self.player_id]
+        players_distance = self.euclidean_distance(self.coordinates(own_loc), self.coordinates(opp_loc))
+        own_moves_minus_opp_moves = self.score(state)
+        own_distance_from_center= self.distance_from_center(self.coordinates(own_loc))
+        return own_moves_minus_opp_moves - own_distance_from_center + players_distance
+    
+    def coordinates(self, encoded_location):
+        """
+        Gets x,y coordinates out of the encoded location
+        """
+        x = encoded_location % 13 
+        y = math.floor(encoded_location/13) 
+        return x, y
+
+    def distance_from_center(self, player_location):
+        """
+        Calculating the Euclidean distance to centre
+        """
+        centre_location= 5, 4
+        return self.euclidean_distance(player_location, centre_location)
+    
+    def euclidean_distance(self, loc1, loc2):
+        """
+        Returns the manhattan distance between two points (loc1 and loc2)
+        """
+        dx = abs(loc1[0] - loc2[0])
+        dy = abs(loc1[1] - loc2[1])
+        return math.sqrt(dx * dx + dy * dy)
